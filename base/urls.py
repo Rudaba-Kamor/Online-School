baf0ac1fe821d2a28cc75f3378c7e8d5deed76af
Rudaba-Kamor <rudabaadninkamor@gmail.com>
2023-07-21 01:39:56 +0600

    commit

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name= 'home'),
+    path('classroom/', views.classroom, name= 'classroom'),
+    path('blog/', views.blog, name= 'blog'),
+    path('contact/', views.contact, name= 'contact'),
+    path('dashboard/', views.dashboard, name= 'dashboard'),
+    path('about/', views.about, name= 'about'),
 ]
--- base/views.py
+++ base/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+def classroom(request):
+    return render(request, 'classroom.html')
+def blog(request):
+    return render(request, 'blog.html')
+def contact(request):
+    return render(request, 'contact.html')
+def dashboard(request):
+    return render(request, 'dashboard.html')
+def about(request):
+    return render(request, 'about.html')
